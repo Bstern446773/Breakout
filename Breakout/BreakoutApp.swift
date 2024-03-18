b98be4eae054a8a3aad55fcb3000e769293e52f7
Bennett Stern <bstern@bsd220.org>
2024-03-18 09:31:29 -0500

    Initial Commit

---
+++ Breakout/BreakoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakoutApp.swift
+//  Breakout
+//
+//  Created by Bennett Stern on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BreakoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
